f0fc8370230d3dd4334b7d5ceebc2752e180bfe6
Colin <colinjdoherty@outlook.com>
2022-03-29 22:07:52 +0000

    unfinished

--- animation.js
+++ animation.js
@@ -2,12 +2,23 @@ const screen = document.querySelector('#screen');
 let height;
 let width;
 const ctx = screen.getContext('2d');
-const sizeScreen = (function() {
+const sizeScreen = function() {
 	height = window.innerHeight;
 	width = window.innerWidth;
 	screen.height = height;
 	screen.width = width;
-})()
+}
 
-ctx.fillStyle = 'rgb(0,0,0)';
-ctx.fillRect(0, 0, width, height);
+const startAnimation = function(){
+	let start = Date.now();
+	let end = Date.now();
+	function frame(time){
+		console.log(time)
+		ctx.fillStyle = 'rgb(0,0,0)';
+		ctx.fillRect(0, 0, width, height);	
+		Player.drawEveryone();
+		end = Date.now();
+		requestAnimationFrame(frame);
+	}
+	requestAnimationFrame(frame);
+}
